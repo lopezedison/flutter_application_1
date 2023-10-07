be5d6f1257339d9dc7446507178049ab7e05fb26
David Cardenas <alzadavid10@gmail.com>
2023-10-07 12:39:38 -0500

    Actualizacion, modificaccion de las clases, en doc

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -84,7 +85,13 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const PrincipalContactos()),
+                    );
+                  },
                   child: const Text('Log In'),
                 ),
                 ElevatedButton(
@@ -182,6 +189,290 @@ class InfoPerfil extends StatelessWidget {
               ],
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class PrincipalContactos extends StatelessWidget {
+  const PrincipalContactos({super.key});
+
+  get screenHeight => null;
+  get screenWidth => null;
+
+  @override
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    double iconSizeButt = screenWidth * 0.2;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 151, 206, 251),
+        title: const Text('Contactos'),
+      ),
+      body: Column(
+        children: <Widget>[
+          // División superior para botones de navegación
+          Container(
+            color: const Color.fromARGB(
+                255, 151, 206, 251), // Color de fondo de la división superior
+            height: 80, // Altura de la división superior
+            child: const Center(),
+          ),
+          // División central para mostrar contenido
+          Expanded(
+            child: Container(
+              color: const Color.fromARGB(
+                  255, 255, 255, 255), // Color de fondo de la división central
+              child: const Center(),
+            ),
+          ),
+          // División inferior para botones de imagen
+          Container(
+            color: const Color.fromARGB(
+                255, 255, 255, 255), // Color de fondo de la división inferior
+            height: 100, // Altura de la división inferior
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  // Botón 1
+                  IconButton(
+                    icon: Image.asset(
+                      'lib/icons/contactos.png',
+                      width: iconSizeButt,
+                      height: iconSizeButt,
+                    ),
+                    iconSize: iconSizeButt,
+                    onPressed: () {
+                      // Acción para el botón 1
+                    },
+                  ),
+                  // Botón 2
+                  IconButton(
+                    icon: Image.asset(
+                      'lib/icons/watsapp2.png',
+                      width: iconSizeButt,
+                      height: iconSizeButt,
+                    ),
+                    iconSize: iconSizeButt,
+                    onPressed: () {
+                      // Acción para el botón 2
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const PrincipalWhatsApp()),
+                      );
+                    },
+                  ),
+                  // Botón 3
+                  IconButton(
+                    icon: Image.asset(
+                      'lib/icons/seg2.png',
+                      width: iconSizeButt,
+                      height: iconSizeButt,
+                    ),
+                    onPressed: () {
+                      // Acción para el botón 3
+                    },
+                  ),
+                  // Botón 4
+                  IconButton(
+                    icon: Image.asset(
+                      'lib/icons/docs2.png',
+                      width: iconSizeButt,
+                      height: iconSizeButt,
+                    ),
+                    iconSize: iconSizeButt,
+                    onPressed: () {
+                      // Acción para el botón 4
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PrincipalWhatsApp extends StatelessWidget {
+  const PrincipalWhatsApp({super.key});
+
+  get screenHeight => null;
+  get screenWidth => null;
+
+  @override
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    double iconSizeButt = screenWidth * 0.2;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 151, 206, 251),
+        title: const Text('WhatsApp'),
+      ),
+      body: Column(
+        children: <Widget>[
+          // División superior para botones de navegación
+          Container(
+            color: const Color.fromARGB(
+                255, 151, 206, 251), // Color de fondo de la división superior
+            height: 80, // Altura de la división superior
+            child: const Center(),
+          ),
+          // División central para mostrar contenido
+          Expanded(
+            child: Container(
+              color: const Color.fromARGB(
+                  255, 255, 255, 255), // Color de fondo de la división central
+              child: const Center(),
+            ),
+          ),
+          // División inferior para botones de imagen
+          Container(
+            color: const Color.fromARGB(
+                255, 255, 255, 255), // Color de fondo de la división inferior
+            height: 100, // Altura de la división inferior
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  // Botón 1
+                  IconButton(
+                    icon: Image.asset(
+                      'lib/icons/contactos2.png',
+                      width: iconSizeButt,
+                      height: iconSizeButt,
+                    ),
+                    iconSize: iconSizeButt,
+                    onPressed: () {
+                      // Acción para el botón 1
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const PrincipalContactos()),
+                      );
+                    },
+                  ),
+                  // Botón 2
+                  IconButton(
+                    icon: Image.asset(
+                      'lib/icons/watsapp1.png',
+                      width: iconSizeButt,
+                      height: iconSizeButt,
+                    ),
+                    iconSize: iconSizeButt,
+                    onPressed: () {
+                      // Acción para el botón 2
+                    },
+                  ),
+                  // Botón 3
+                  IconButton(
+                    icon: Image.asset(
+                      'lib/icons/seg2.png',
+                      width: iconSizeButt,
+                      height: iconSizeButt,
+                    ),
+                    onPressed: () {
+                      // Acción para el botón 3
+                    },
+                  ),
+                  // Botón 4
+                  IconButton(
+                    icon: Image.asset(
+                      'lib/icons/docs2.png',
+                      width: iconSizeButt,
+                      height: iconSizeButt,
+                    ),
+                    iconSize: iconSizeButt,
+                    onPressed: () {
+                      // Acción para el botón 4
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ModeloBase extends StatelessWidget {
+  const ModeloBase({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          // División superior para botones de navegación
+          Container(
+            color: Colors.blue, // Color de fondo de la división superior
+            height: 80, // Altura de la división superior
+            child: const Center(
+              child: Text('Contenido superior'),
+            ),
+          ),
+          // División central para mostrar contenido
+          Expanded(
+            child: Container(
+              color: Colors.green, // Color de fondo de la división central
+              child: const Center(
+                child: Text('Contenido del centro'),
+              ),
+            ),
+          ),
+          // División inferior para botones de imagen
+          Container(
+            color: Colors.red, // Color de fondo de la división inferior
+            height: 100, // Altura de la división inferior
+            child: const Center(
+              child: Text('Contenido inferior'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: <Widget>[
+              GestureDetector(
+                onTap: () {
+                  // Navigate to the second page when tapped on the image
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SecondPage()),
+                  );
+                },
+                child: Image.network(
+                  'https://via.placeholder.com/200',
+                  width: 200.0,
+                  height: 200.0,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
