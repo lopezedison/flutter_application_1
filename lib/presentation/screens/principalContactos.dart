7aa3aacd20ef49f4d4d6a9353471bb0bc64073d9
David Cardenas <alzadavid10@gmail.com>
2023-10-19 18:53:10 -0500

    stack widget

--- lib/presentation/screens/principalContactos.dart
+++ lib/presentation/screens/principalContactos.dart
@@ -23,129 +23,207 @@ class PrincipalContactos extends StatelessWidget {
         backgroundColor: const Color.fromARGB(255, 151, 206, 251),
         title: const Text('Contactos'),
       ),
-      body: Column(
-        children: <Widget>[
-          // División superior para botones de navegación
-          Container(
-            color: const Color.fromARGB(255, 151, 206, 251),
-            height: 80,
-            child: Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  IconButton(
-                    icon: Image.asset(
-                      'assets/icons/perfil1.png',
-                      width: iconSizePerf,
-                      height: iconSizePerf,
-                    ),
-                    onPressed: () {
-                      // Acción al presionar el botón
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
-          // División central para mostrar contenido
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
+        children: [
+          Column(
+            children: <Widget>[
+              // División superior para botones de navegación
+              Container(
+                color: const Color.fromARGB(255, 151, 206, 251),
+                height: 80,
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
                     children: <Widget>[
                       IconButton(
                         icon: Image.asset(
-                          'assets/icons/recientes.png',
-                          width: screenWidth * 0.1,
-                          height: screenWidth * 0.1,
+                          'assets/icons/perfil1.png',
+                          width: iconSizePerf,
+                          height: iconSizePerf,
                         ),
                         onPressed: () {
-                          // Acción para el botón 1
-                        },
-                      ),
-                      IconButton(
-                        icon: Image.asset(
-                          'assets/icons/libreta.png',
-                          width: screenWidth * 0.1,
-                          height: screenWidth * 0.1,
-                        ),
-                        onPressed: () {
-                          // Acción para el botón 1
+                          // Acción al presionar el botón
                         },
                       ),
                     ],
                   ),
-                  const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.blue,
-                        hintText: 'Buscar contactos',
-                        hintStyle: TextStyle(
-                          color: Colors.white,
-                        ),
-                        prefixIcon: Icon(
-                          Icons.search,
-                          color: Colors.white,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  IconButton(
+                    icon: Image.asset(
+                      'assets/icons/recientes.png',
+                      width: screenWidth * 0.1,
+                      height: screenWidth * 0.1,
+                    ),
+                    onPressed: () {
+                      // Acción para el botón 1
+                    },
+                  ),
+                  IconButton(
+                    icon: Image.asset(
+                      'assets/icons/libreta.png',
+                      width: screenWidth * 0.1,
+                      height: screenWidth * 0.1,
+                    ),
+                    onPressed: () {
+                      // Acción para el botón 1
+                    },
+                  ),
+                ],
+              ),
+              // División central para mostrar contenido
+              Expanded(
+                child: ListView(
+                  children: <Widget>[
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.blue,
+                          hintText: 'Buscar contactos',
+                          hintStyle: TextStyle(
+                            color: Colors.white,
+                          ),
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
+                    const ListTile(
+                      title: Text('Contacto 1'),
+                      subtitle: Text('Número de teléfono'),
+                    ),
+                    const ListTile(
+                      title: Text('Contacto 1'),
+                      subtitle: Text('Número de teléfono'),
+                    ),
+                    const ListTile(
+                      title: Text('Contacto 1'),
+                      subtitle: Text('Número de teléfono'),
+                    ),
+                    const ListTile(
+                      title: Text('Contacto 1'),
+                      subtitle: Text('Número de teléfono'),
+                    ),
+                    const ListTile(
+                      title: Text('Contacto 1'),
+                      subtitle: Text('Número de teléfono'),
+                    ),
+                    const ListTile(
+                      title: Text('Contacto 1'),
+                      subtitle: Text('Número de teléfono'),
+                    ),
+                    const ListTile(
+                      title: Text('Contacto 1'),
+                      subtitle: Text('Número de teléfono'),
+                    ),
+                    const ListTile(
+                      title: Text('Contacto 1'),
+                      subtitle: Text('Número de teléfono'),
+                    ),
+                    const ListTile(
+                      title: Text('Contacto 1'),
+                      subtitle: Text('Número de teléfono'),
+                    ),
+                    const ListTile(
+                      title: Text('Contacto 1'),
+                      subtitle: Text('Número de teléfono'),
+                    ),
+                    const ListTile(
+                      title: Text('Contacto 1'),
+                      subtitle: Text('Número de teléfono'),
+                    ),
+                    // Resto de los elementos de la lista
+                  ],
+                ),
+              ), // División inferior para botones de imagen
+              Container(
+                color: const Color.fromARGB(255, 255, 255,
+                    255), // Color de fondo de la división inferior
+                height: 100, // Altura de la división inferior
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      // Botón 1
+                      IconButton(
+                        icon: Image.asset(
+                          'assets/icons/contactos.png',
+                          width: iconSizeButt,
+                          height: iconSizeButt,
+                        ),
+                        iconSize: iconSizeButt,
+                        onPressed: () {
+                          // Acción para el botón 1
+                        },
+                      ),
+                      // Botón 2
+                      IconButton(
+                        icon: Image.asset(
+                          'assets/icons/watsapp2.png',
+                          width: iconSizeButt,
+                          height: iconSizeButt,
+                        ),
+                        iconSize: iconSizeButt,
+                        onPressed: () {
+                          // Acción para el botón 2
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    const PrincipalWhatsApp()),
+                          );
+                        },
+                      ),
+                      // Botón 3
+                      IconButton(
+                        icon: Image.asset(
+                          'assets/icons/seg2.png',
+                          width: iconSizeButt,
+                          height: iconSizeButt,
+                        ),
+                        onPressed: () {
+                          // Acción para el botón 3
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    const PaginaActividades()),
+                          );
+                        },
+                      ),
+                      // Botón 4
+                      IconButton(
+                        icon: Image.asset(
+                          'assets/icons/docs2.png',
+                          width: iconSizeButt,
+                          height: iconSizeButt,
+                        ),
+                        iconSize: iconSizeButt,
+                        onPressed: () {
+                          // Acción para el botón 4
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const PaginaDocumentos()),
+                          );
+                        },
+                      ),
+                    ],
                   ),
-                  const ListTile(
-                    title: Text('Contacto 1'),
-                    subtitle: Text('Número de teléfono'),
-                  ),
-                  const ListTile(
-                    title: Text('Contacto 1'),
-                    subtitle: Text('Número de teléfono'),
-                  ),
-                  const ListTile(
-                    title: Text('Contacto 1'),
-                    subtitle: Text('Número de teléfono'),
-                  ),
-                  const ListTile(
-                    title: Text('Contacto 1'),
-                    subtitle: Text('Número de teléfono'),
-                  ),
-                  const ListTile(
-                    title: Text('Contacto 1'),
-                    subtitle: Text('Número de teléfono'),
-                  ),
-                  const ListTile(
-                    title: Text('Contacto 1'),
-                    subtitle: Text('Número de teléfono'),
-                  ),
-                  const ListTile(
-                    title: Text('Contacto 1'),
-                    subtitle: Text('Número de teléfono'),
-                  ),
-                  const ListTile(
-                    title: Text('Contacto 1'),
-                    subtitle: Text('Número de teléfono'),
-                  ),
-                  const ListTile(
-                    title: Text('Contacto 1'),
-                    subtitle: Text('Número de teléfono'),
-                  ),
-                  const ListTile(
-                    title: Text('Contacto 1'),
-                    subtitle: Text('Número de teléfono'),
-                  ),
-                  const ListTile(
-                    title: Text('Contacto 1'),
-                    subtitle: Text('Número de teléfono'),
-                  ),
-                  // Resto de los elementos de la lista
-                ],
+                ),
               ),
-            ),
+            ],
           ),
           Positioned(
-            right: 70,
-            bottom: 300,
+            right: 30,
+            bottom: 90,
             child: IconButton(
               icon: Image.asset(
                 'assets/icons/tecladonum.png',
@@ -186,85 +264,7 @@ class PrincipalContactos extends StatelessWidget {
               },
             ),
           ),
-          // División inferior para botones de imagen
-          Container(
-            color: const Color.fromARGB(
-                255, 255, 255, 255), // Color de fondo de la división inferior
-            height: 100, // Altura de la división inferior
-            child: Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  // Botón 1
-                  IconButton(
-                    icon: Image.asset(
-                      'assets/icons/contactos.png',
-                      width: iconSizeButt,
-                      height: iconSizeButt,
-                    ),
-                    iconSize: iconSizeButt,
-                    onPressed: () {
-                      // Acción para el botón 1
-                    },
-                  ),
-                  // Botón 2
-                  IconButton(
-                    icon: Image.asset(
-                      'assets/icons/watsapp2.png',
-                      width: iconSizeButt,
-                      height: iconSizeButt,
-                    ),
-                    iconSize: iconSizeButt,
-                    onPressed: () {
-                      // Acción para el botón 2
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const PrincipalWhatsApp()),
-                      );
-                    },
-                  ),
-                  // Botón 3
-                  IconButton(
-                    icon: Image.asset(
-                      'assets/icons/seg2.png',
-                      width: iconSizeButt,
-                      height: iconSizeButt,
-                    ),
-                    onPressed: () {
-                      // Acción para el botón 3
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const PaginaActividades()),
-                      );
-                    },
-                  ),
-                  // Botón 4
-                  IconButton(
-                    icon: Image.asset(
-                      'assets/icons/docs2.png',
-                      width: iconSizeButt,
-                      height: iconSizeButt,
-                    ),
-                    iconSize: iconSizeButt,
-                    onPressed: () {
-                      // Acción para el botón 4
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const PaginaDocumentos()),
-                      );
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
         ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
       ),
     );
   }
